612ed256c1001858b022bfa2f06205591b348000
sweetwasabi-KubSU <gitsweetwasabi@gmail.com>
2021-04-11 14:33:01 +0300

    added task 13.5

--- laboratory work №13/classical-algorithms/src/Task1.kt
+++ laboratory work №13/classical-algorithms/src/Task1.kt
@@ -1,5 +1,5 @@
 fun main(args: Array<String>) {
-   task3()
+   task5()
 }
 
 /* Установить Intellij Idea, JDK и плагин для Kotlin
@@ -9,8 +9,37 @@ fun task1() = println("Hello, World!")
 /* Принять имя пользователя как аргумент программы
    Поздороваться с пользователем. Применить форматирование строки */
 fun task3() {
-   print("Enter the user name: ")
-   val name = readLine()
-
+   val name = inputName()
    println("Hello, $name!")
 }
+
+// ввод имени пользователя
+fun inputName(): String? {
+   print("Enter the user name: ")
+   return readLine()
+}
+
+/* Продолжение задания 3: Спросить у пользователя, какой язык
+   у него любимый, если это Kotlin или Prolog, ответь пользователю,
+   что он — подлиза, для других языков придумать комментарий,
+   воспользоваться для решения задачи условным оператором и операторм when */
+fun task5() {
+   val name = inputName()
+
+   println("\n$name, which is your favorite programming language?")
+   print(":> ")
+
+   val language: String? = readLine()
+   print("\n${chooseAnswerByLanguage(language)}")
+}
+
+// выбор ответа в зависимости от языка
+fun chooseAnswerByLanguage(language: String?) =
+   when(language) {
+      "Prolog","Kotlin" -> "You're licksplittle!"
+      "C#" -> "Buddy, it's time to start smth new!"
+      "C++" -> "I'm sorry, but working with classes on it isn't very convenient!"
+      "F#" -> "No, you have Kotlin, so third course won't be able to help"
+      "Pascal" -> "You're doing good, kid!"
+      else -> "Don't show off like you're the boss"
+   }
