bd4424890ae449c324be1af320f122c78606af03
sweetwasabi-KubSU <gitsweetwasabi@gmail.com>
2021-04-24 04:54:03 +0300

    added task 14.2

--- laboratory work №14/Recursion/src/Numbers.kt
+++ laboratory work №14/Recursion/src/Numbers.kt
@@ -4,7 +4,10 @@ fun main() {
     val number = inputNumber()
 
     // task 1
-    println("Sum of digits (recursion up): ${sumDigitsUp(number)}")
+    // println("Sum of digits (recursion up): ${sumDigitsUp(number)}")
+
+    // task 2
+    println("Sum of digits (tail recursion): ${sumDigitsTail(number)}")
 }
 
 // ввод числа
@@ -26,3 +29,13 @@ fun sumDigitsUp(number: Int): Int =
         abs(number % 10)
     else
         sumDigitsUp(number / 10) + abs(number % 10)
+
+// task 2: сумма цифр числа (хвостовая рекурсия)
+fun sumDigitsTail(number: Int, sum: Int): Int =
+    if (number / 10 == 0)
+        sum + abs(number % 10)
+    else
+        sumDigitsTail(number / 10, sum + abs(number % 10))
+
+fun sumDigitsTail(number: Int): Int =
+    sumDigitsTail(number, 0)
