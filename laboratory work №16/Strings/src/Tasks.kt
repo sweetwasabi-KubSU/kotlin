af61278bec583d172e244d5ce447600c8d0ec12f
sweetwasabi-KubSU <gitsweetwasabi@gmail.com>
2021-05-22 12:04:10 +0300

    added task 16.2.3 (1/3)

--- laboratory work №16/Strings/src/Tasks.kt
+++ laboratory work №16/Strings/src/Tasks.kt
@@ -1,9 +1,17 @@
+import kotlin.random.Random
+
 fun main() {
-    task1()
+    print("Input string: ")
+    val s = readLine()!!.toString()
+
+    // task1()
+
+    // task 2: задачи 3, 8, 16
+    task2_3(s)
 }
 
-// task 1: Дана строка в которой числа перечислены через пробел,
-// необходимо найти максимальное из этих чисел
+// task 1: дана строка в которой числа перечислены
+// через пробел, найти максимальное из этих чисел
 fun task1() {
     print("Input string: ")
     val s = readLine()
@@ -17,4 +25,19 @@ fun task1() {
         println("Error: ${e.message}. Try again!\n")
         task1()
     }
+}
+
+// task 2.3: дана строка в которой слова записаны через пробел,
+// перемешать все слова этой строке в случайном порядке
+fun task2_3(s: String) {
+    fun replace(list: MutableList<String>, i: Int, j: Int) {
+        val temp = list[i]
+        list[i] = list[j]
+        list[j] = temp
+    }
+
+    val listOfWords = s.split(" ").toMutableList()
+    listOfWords.mapIndexed { i, _ ->  replace(listOfWords, i, Random.nextInt(0, listOfWords.size)) }
+
+    println("Shuffled string: ${listOfWords.joinToString(" ")}")
 }
