50ad72ec2ab728707b124db34510d1cc58253357
sweetwasabi-KubSU <gitsweetwasabi@gmail.com>
2021-05-30 13:52:05 +0300

    added task 17.2

--- laboratory work №17/Classes/src/DrivingLicense.kt
+++ laboratory work №17/Classes/src/DrivingLicense.kt
@@ -1,7 +1,33 @@
 import java.util.*
 
-class DrivingLicense (val surname: String, val name: String, val patronymic: String,
-                      val dateOfBirth: Date, val cityOfBirth: String,
-                      val dateOfIssue: Date, val expiryDate: Date, val policeDepartment: String,
-                      private val licenseSeries: String, private val licenseNumber: String,
-                      val cityOfIssue: String, val categories: List<String>, val yearOfDrivingExperience: Int )
+class DrivingLicense(private val surname: String,
+    private val name: String, private val patronymic: String,
+    private val dateOfBirth: Date, private val placeOfBirth: String,
+    private val dateOfIssue: Date, private val expiryDate: Date,
+    private val policeDepartment: String,
+    private val licenseSeries: String, private val licenseNumber: String,
+    private val placeOfIssue: String,
+    private val categories: List<String>,
+    private val startingYearOfDriving: Int) {
+
+    fun printDrivingLicense() {
+        println("ВОДИТЕЛЬСКОЕ УДОСТОВЕРЕНИЕ\n")
+        println("1\t$surname")
+        println("2\t$name $patronymic")
+        println("3\t${getDate(dateOfBirth)}")
+        println("\t$placeOfBirth")
+        println("4a)\t${getDate(dateOfIssue)}\t4b)\t${getDate(expiryDate)}")
+        println("4c)\t$policeDepartment")
+        println("5\t$licenseSeries $licenseNumber")
+        // 8, потому что 6 и 7 - фото и подпись
+        println("8\t$placeOfIssue")
+        println("9\t${categories.joinToString(" ")}")
+        // 14, потому что до этого - специальные поля
+        println("14\tСТАЖ С $startingYearOfDriving")
+    }
+
+    private fun getDate(date: Date): String {
+        val check = {date: Int -> if (date < 10) "0$date" else "$date"}
+        return "${check(date.date)}.${check(date.month)}.${date.year}"
+    }
+}
--- laboratory work №17/Classes/src/main.kt
+++ laboratory work №17/Classes/src/main.kt
@@ -1,9 +1,13 @@
 import java.util.*
 
 fun main() {
+    // task 1
     val diana = DrivingLicense("ПИВОВАРОВА", "ДИАНА", "ШАМИЛЬЕВНА",
         Date(2000, 3, 28), "КРАСНОДАРСКИЙ КР.",
         Date(2019, 6, 27), Date(2029, 6, 27),
         "ГИБДД 2303", "0123", "456789",
         "КРАСНОДАРСКИЙ КР.", listOf("B", "B1", "M"), 2019)
+
+    // task 2
+    diana.printDrivingLicense()
 }
