0d6f592d2014aceb12a0b321ef8232328fbc6d18
sweetwasabi-KubSU <gitsweetwasabi@gmail.com>
2021-06-05 05:33:09 +0300

    added tasks 17.3, 17.4

--- laboratory work №17/Classes/src/DrivingLicense.kt
+++ laboratory work №17/Classes/src/DrivingLicense.kt
@@ -2,32 +2,58 @@ import java.util.*
 
 class DrivingLicense(private val surname: String,
     private val name: String, private val patronymic: String,
-    private val dateOfBirth: Date, private val placeOfBirth: String,
-    private val dateOfIssue: Date, private val expiryDate: Date,
+    private val dateOfBirth: MyDate, private val placeOfBirth: String,
+    private val dateOfIssue: MyDate, private val expiryDate: MyDate,
     private val policeDepartment: String,
     private val licenseSeries: String, private val licenseNumber: String,
     private val placeOfIssue: String,
     private val categories: List<String>,
-    private val startingYearOfDriving: Int) {
+    private val drivingExperience: Int): Comparable<DrivingLicense> {
 
     fun printDrivingLicense() {
         println("ВОДИТЕЛЬСКОЕ УДОСТОВЕРЕНИЕ\n")
         println("1\t$surname")
         println("2\t$name $patronymic")
-        println("3\t${getDate(dateOfBirth)}")
+        println("3\t${dateOfBirth.getDateToPrint()}")
         println("\t$placeOfBirth")
-        println("4a)\t${getDate(dateOfIssue)}\t4b)\t${getDate(expiryDate)}")
+        println("4a)\t${dateOfIssue.getDateToPrint()}\t4b)\t${expiryDate.getDateToPrint()}")
         println("4c)\t$policeDepartment")
         println("5\t$licenseSeries $licenseNumber")
         // 8, потому что 6 и 7 - фото и подпись
         println("8\t$placeOfIssue")
         println("9\t${categories.joinToString(" ")}")
         // 14, потому что до этого - специальные поля
-        println("14\tСТАЖ С $startingYearOfDriving")
+        println("14\tСТАЖ С $drivingExperience")
     }
 
-    private fun getDate(date: Date): String {
-        val check = {date: Int -> if (date < 10) "0$date" else "$date"}
-        return "${check(date.date)}.${check(date.month)}.${date.year}"
+    // task 3
+    // Если этот метод возвращает отрицательное число,
+    // то текущий объект будет располагаться перед тем, который передается через параметр;
+    // Если метод вернет положительное число, то, наоборот, после второго объекта;
+    // Если метод возвратит ноль, значит, оба объекта равны;
+    override fun compareTo(other: DrivingLicense): Int {
+        return when {
+            this.licenseSeries > other.licenseSeries -> -1
+            this.licenseSeries < other.licenseSeries -> 1
+            else -> {
+                when {
+                    this.licenseNumber > other.licenseNumber -> -1
+                    this.licenseNumber < other.licenseNumber -> 1
+                    else -> 0
+                }
+            }
+        }
+
     }
+
+    // task 4
+    private fun isSurnameCorrect(): Boolean =
+        """^[А-Я][а-я]+$""".toRegex().find(surname) != null
+
+    private fun isNameCorrect(): Boolean =
+        """^[А-Я][а-я]+$""".toRegex().find(name) != null
+
+    private fun isPatronymicCorrect(): Boolean =
+        """^[А-Я][а-я]+$""".toRegex().find(patronymic) != null
+
 }
